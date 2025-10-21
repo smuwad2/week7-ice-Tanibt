d39f3d6066671b34a5742ce8a558451221cb5093
Tanish <65279774+Tanibt@users.noreply.github.com>
2025-10-21 02:20:30 +0000

    ex2-test-works

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,18 +1,42 @@
 <script>
-    export default { 
-        // Add code in appropriate places
-        data() {
-            return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+export default {
+    // Add code in appropriate places
+    data() {
+        return {
+            items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+            newItem: ''
+        }
+
+    },
+    methods: {
+        addItem() {
+            if (this.newItem !== '') {
+                this.items.push(this.newItem)
+                this.newItem = ''
+
             }
-            
+        },
+        deleteItem(index) {
+            this.items.splice(index, 1)
         }
     }
+}
 </script>
 
 <template>
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
-   
+    <input v-model="newItem" placeholder="Enter item name" />
+    <button @click="addItem">Add!</button>
+
+    <ul>
+        <li v-for="(item, index) in items" :key="index">
+            {{ item }}
+            <button @click="deleteItem(index)">Delete</button>
+        </li>
+    </ul>
+
+
+
 </template>
